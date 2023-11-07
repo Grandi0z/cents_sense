857e53a390c462092cdf0fd310a36bca5d3fdb04
Joseph Lwanzo Khausi <josephlkausi@gmail.com>
2023-11-07 13:19:44 +0200

    Add controllers

---
+++ app/controllers/expense_category_controller.rb
@@ -0,0 +1,22 @@
+class ExpenseCategoryController < ApplicationController
+  def new
+  end
+
+  def crate
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
---
+++ app/controllers/expense_controller.rb
@@ -0,0 +1,22 @@
+class ExpenseController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def destrol
+  end
+end
