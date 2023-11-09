72256f1ddc54d2f1c912eaad2ee3c819b51271c7
Joseph Lwanzo Khausi <josephlkausi@gmail.com>
2023-11-09 17:05:04 +0200

    Style sign up and out views

--- app/models/expense_category.rb
+++ app/models/expense_category.rb
@@ -1,3 +1,5 @@
 class ExpenseCategory < ApplicationRecord
-    
+  validates :name, presence: true, length: { in: 2..250 }
+  has_many :expenses
+  has_one_attached :icon
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   validates :name, presence: true
-  has_many :expense_categories, foreign_key: 'author_id'
+  has_many :expense_categories
   has_many :expenses, foreign_key: 'author_id'
 
   def admin?; end
