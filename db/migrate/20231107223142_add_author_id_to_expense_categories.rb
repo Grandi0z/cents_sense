e287a71330097e272e7cdfe1646c3ce2a86919ee
Joseph Lwanzo Khausi <josephlkausi@gmail.com>
2023-11-11 19:36:50 +0200

    Update 20231107223142_add_author_id_to_expense_categories.rb

--- db/migrate/20231107223142_add_author_id_to_expense_categories.rb
+++ db/migrate/20231107223142_add_author_id_to_expense_categories.rb
@@ -1,5 +1,5 @@
 class AddAuthorIdToExpenseCategories < ActiveRecord::Migration[7.0]
   def change
-      add_reference :expense_categories, :user, foreign_key: { to_table: :users }, column: :author_id
+      add_reference :expense_categories, :user, foreign_key: { to_table: :users }, column: :user_id
   end
 end
