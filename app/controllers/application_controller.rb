7d2067e14747b3d29728b7ab98ad5c84c0b2239e
Joseph Lwanzo Khausi <josephlkausi@gmail.com>
2023-11-12 05:09:47 +0200

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   require 'cancancan'
-  # Catch all CanCan errors and alert the user of the exception
-  # rescue_from CanCanCan::AccessDenied do |exception|
-  #   redirect_to root_url, alert: exception.message
-  # end
-
   protected
 
   def configure_permitted_parameters
